7faaac5ec1221035112f8ac972fd6aa7331d1218
Inumedia <inumedia@inumedia.net>
2014-06-11 21:31:16 -0500

    Adding solution file, because Visual Studio.
    Also, adding Assembly information.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SlackAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Slack wrapper for direct interaction with their APIs.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Inumedia")]
 [assembly: AssemblyProduct("SlackAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
