f67241b353245d9bfbb7fb2d6b4468aaebcf43e1
Inumedia <inumedia@inumedia.net>
2014-07-10 14:32:22 -0500

    Fixing NewMessages

--- WebSocketMessages/NewMessage.cs
+++ WebSocketMessages/NewMessage.cs
@@ -5,7 +5,10 @@ namespace SlackAPI.WebSocketMessages
     [SlackSocketRouting("message")]
     public class NewMessage : SlackSocketMessage
     {
+        public string user;
         public string channel;
         public string text;
+        public string team;
+        public DateTime ts;
     }
 }
