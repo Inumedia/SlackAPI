9d451d21e56caa6248590e08248e38181a675111
Inumedia <inumedia@inumedia.net>
2015-06-20 14:37:11 -0400

    Updating tests

--- IntegrationTest/Connect.cs
+++ IntegrationTest/Connect.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SlackAPI;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 
 namespace IntegrationTest
@@ -13,15 +15,37 @@ namespace IntegrationTest
         {
             EventWaitHandle wait = new EventWaitHandle(false, EventResetMode.ManualReset);
 
-            SlackSocketClient client = new SlackSocketClient("AUTH_TOKEN");
-            client.Connect((c) =>
+            SlackSocketClient client = new SlackSocketClient("USER_TOKEN");
+            client.Connect((o) =>
             {
+                Debug.WriteLine("RTM Start");
+            }, () =>
+            {
+                Debug.WriteLine("Connected");
                 wait.Set();
             });
 
-            Assert.IsTrue(wait.WaitOne(1000) || client.IsConnected);
+            Assert.IsTrue(wait.WaitOne(10000), "Didn't return within a reasonable time.");
+            Assert.IsTrue(client.IsConnected, "Invalid, doesn't think it's connected.");
+        }
 
-            Console.WriteLine("Wooo");
+        [TestMethod]
+        public void TestConnectBot()
+        {
+            EventWaitHandle wait = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+            SlackSocketClient client = new SlackSocketClient("BOT_TOKEN");
+            client.Connect((o) =>
+            {
+                Debug.WriteLine("RTM Start");
+            }, () =>
+            {
+                Debug.WriteLine("Connected");
+                wait.Set();
+            });
+
+            Assert.IsTrue(wait.WaitOne(10000), "Didn't return within a reasonable time.");
+            Assert.IsTrue(client.IsConnected, "Invalid, doesn't think it's connected.");
         }
     }
 }
