c3d7603381d2971db571a834e1fafbe73ac203d1
Brant Bobby <brantb@iqmetrix.com>
2017-06-02 18:02:45 -0500

    Added basic integration test for GetUserList

---
+++ SlackAPI.Tests/Users.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using SlackAPI.Tests.Configuration;
+using SlackAPI.Tests.Helpers;
+using Xunit;
+
+namespace SlackAPI.Tests
+{
+    [Collection("Integration tests")]
+    public class Users
+    {
+        private readonly IntegrationFixture fixture;
+
+        public Users(IntegrationFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+        [Fact]
+        public void UserList()
+        {
+            var client = this.fixture.UserClient;
+            UserListResponse actual = null;
+            using (var sync = new InSync(nameof(SlackClient.UserLookup)))
+            {
+                client.GetUserList(response =>
+                {
+                    actual = response;
+                    sync.Proceed();
+                });
+            }
+
+            Assert.True(actual.ok, "Error while fetching user list.");
+            Assert.True(actual.members.Any());
+        }
+    }
+}
