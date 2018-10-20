b866207b51a887af987b1881ebe15ba11263743f
Gregoire Pailler <gregoire@pailler.fr>
2018-10-21 00:29:10 +0700

    Readded netstandard 1.6 because netstandard 1.3 cannot detect RoutinMessage in custom assemblies

--- SlackAPI/SlackSocket.cs
+++ SlackAPI/SlackSocket.cs
@@ -10,6 +10,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 
+#if NETSTANDARD1_6	
+using Microsoft.Extensions.DependencyModel;	
+#endif
+
 namespace SlackAPI
 {
     public class SlackSocket
@@ -39,8 +43,13 @@ namespace SlackAPI
             routing = new Dictionary<string, Dictionary<string, Type>>();
 #if NET45
              var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(x => x.GlobalAssemblyCache == false);
-#else
+#elif NETSTANDARD1_6
+             var assemblies = DependencyContext.Default.GetDefaultAssemblyNames().Select(Assembly.Load);
+#elif NETSTANDARD1_3
             var assemblies = new[] { typeof(SlackSocket).GetType().GetTypeInfo().Assembly };
+#warning Routing messages in custom assemblies are not supported with .Net Standard 1.3
+#else
+#error Platform not supported
 #endif
             foreach (Assembly assembly in assemblies)
             {
