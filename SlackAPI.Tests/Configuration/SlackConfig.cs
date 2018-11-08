f1d481357a4dc5c8b56321f94a7d6eef9305ad00
Gregoire Pailler <gregoire@pailler.fr>
2018-11-08 12:13:04 +0700

    Rewrite GetAccessToken test using Selenium and remove SlackAPI.Console app

--- SlackAPI.Tests/Configuration/SlackConfig.cs
+++ SlackAPI.Tests/Configuration/SlackConfig.cs
@@ -6,8 +6,12 @@
         public string BotAuthToken { get; set; }
         public string TestChannel { get; set; }
         public string DirectMessageUser { get; set; }
-        public string AuthCode { get; set; }
+
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
+        public string RedirectUrl { get; set; }
+        public string AuthUsername { get; set; }
+        public string AuthPassword { get; set; }
+        public string AuthWorkspace { get; set; }
     }
 }
