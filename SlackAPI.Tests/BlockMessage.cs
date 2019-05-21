b51b4c225d7b833eb471a72b0ff682698bb028f6
clockworkcoding <yodadamax@gmail.com>
2019-05-21 00:27:22 -0400

    Added tests and inheritence to help use block messages

---
+++ SlackAPI.Tests/BlockMessage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using SlackAPI.RPCMessages;
+using SlackAPI.Tests.Configuration;
+using SlackAPI.Tests.Helpers;
+using System.Linq;
+using Xunit;
+
+namespace SlackAPI.Tests
+{
+    [Collection("Integration tests")]
+    public class BlockMessage
+    {
+        private readonly IntegrationFixture fixture;
+
+        public BlockMessage(IntegrationFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+
+        [Fact]
+        public void Blocks()
+        {
+            // given
+            var client = this.fixture.UserClient;
+            PostMessageResponse actual = null;
+
+            // when
+            using (var sync = new InSync(nameof(SlackClient.PostMessage)))
+            {
+                client.PostMessage(
+                    response =>
+                    {
+                        actual = response;
+                        sync.Proceed();
+                    },
+                    this.fixture.Config.TestChannel,
+                    string.Empty,
+                    blocks: SlackMother.SomeBlocks);
+            }
+
+            // then
+            Assert.True(actual.ok, "Error while posting message to channel. ");
+        }
+
+        [Fact]
+        public void BlocksWithActions()
+        {
+            // given
+            var client = this.fixture.UserClient;
+            PostMessageResponse actual = null;
+
+            // when
+            using (var sync = new InSync())
+            {
+                client.PostMessage(
+                    response =>
+                    {
+                        actual = response;
+                        sync.Proceed();
+                    },
+                    this.fixture.Config.TestChannel,
+                    string.Empty,
+                    blocks: SlackMother.SomeBlocksWithActions);
+            }
+
+            // then
+            Assert.True(actual.ok, "Error while posting message to channel. ");
+        }
+    }
+}
--- SlackAPI/SlackClient.cs
+++ SlackAPI/SlackClient.cs
@@ -472,7 +472,7 @@ namespace SlackAPI
             string botName = null,
             string parse = null,
             bool linkNames = false,
-            Block[] blocks = null,
+            IBlock[] blocks = null,
             Attachment[] attachments = null,
             bool as_user = false)
         {
@@ -524,7 +524,7 @@ namespace SlackAPI
             string botName = null,
             string parse = null,
             bool linkNames = false,
-            Block[] blocks = null,
+            IBlock[] blocks = null,
             Attachment[] attachments = null,
             bool unfurl_links = false,
             string icon_url = null,
