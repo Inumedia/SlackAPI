a96b0342d7581a7b4802c28630f59e3d82cb8b1a
TriggerAu <dtregoning@outlook.com.au>
2019-10-16 20:14:55 +1100

    Add UserTyping class and hookup action for user_typing event (Relates #203)
    filled in/renamed the UserTyping method so it matches the reaction/message ones. This now receives the events when a user is typing in a channel

---
+++ SlackAPI/WebSocketMessages/UserTyping.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SlackAPI.WebSocketMessages
+{
+    [SlackSocketRouting("user_typing")]
+    public class UserTyping : SlackSocketMessage
+    {
+        public string user;
+        public string channel;
+    }
+}
