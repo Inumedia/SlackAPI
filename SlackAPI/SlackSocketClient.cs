c99cb74a020e1c69d3edcfd9e15c5a63aaee1d11
Andy <Inumedia@users.noreply.github.com>
2020-08-24 11:50:18 -0400

    Update SlackSocketClient.cs
    Fixing indentation

--- SlackAPI/SlackSocketClient.cs
+++ SlackAPI/SlackSocketClient.cs
@@ -15,7 +15,7 @@ namespace SlackAPI
         public event Action<ReactionAdded> OnReactionAdded;
         public event Action<Pong> OnPongReceived;
         public event Action<PresenceChange> OnPresenceChanged;
-	public event Action OnConnectionLost;
+        public event Action OnConnectionLost;
 
         bool HelloReceived;
         public const int PingInterval = 3000;
@@ -51,10 +51,10 @@ namespace SlackAPI
 
         public void ConnectSocket(Action onSocketConnected){
             underlyingSocket = new SlackSocket(loginDetails, this, onSocketConnected, this.proxySettings);
-	    underlyingSocket.ConnectionClosed += UnderlyingSocket_ConnectionClosed;
+            underlyingSocket.ConnectionClosed += UnderlyingSocket_ConnectionClosed;
         }
-	
-	private void UnderlyingSocket_ConnectionClosed()
+        
+        private void UnderlyingSocket_ConnectionClosed()
         {
             OnConnectionLost?.Invoke();
         }
