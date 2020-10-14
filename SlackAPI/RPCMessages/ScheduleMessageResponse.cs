7658616885868afe94543075a7bd1211bed1b4aa
avuorine <andreas.vuorinen@me.com>
2020-10-14 09:42:25 +0100

    scheduled message support

---
+++ SlackAPI/RPCMessages/ScheduleMessageResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace SlackAPI
+{
+    [RequestPath("chat.scheduleMessage")]
+    public class ScheduleMessageResponse : Response
+    {
+        public string ts;
+        public string channel;
+        public string scheduled_message_id;
+        public int post_at;
+        public Message message;
+
+        public class Message
+        {
+            public string text;
+            public string user;
+            public string username;
+            public string type;
+            public string subtype;
+        }
+    }
+}
