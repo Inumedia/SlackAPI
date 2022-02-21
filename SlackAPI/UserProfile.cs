c93f241d45e8795551273ce38848f0fbb60caffb
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2022-02-21 14:39:30 +0100

    + add title to UserProfile class

--- SlackAPI/UserProfile.cs
+++ SlackAPI/UserProfile.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SlackAPI
+﻿namespace SlackAPI
 {
     public class UserProfile : ProfileIcons
     {
+        public string title;
         public string first_name;
         public string last_name;
         public string real_name;
