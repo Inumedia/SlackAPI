657a7a79bf5c8f764f33b2a5043608ce3536170e
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2022-02-23 17:06:27 +0100

    feat: Add display_name field for UserProfile

--- SlackAPI/UserProfile.cs
+++ SlackAPI/UserProfile.cs
@@ -3,6 +3,7 @@
     public class UserProfile : ProfileIcons
     {
         public string title;
+        public string display_name;
         public string first_name;
         public string last_name;
         public string real_name;
